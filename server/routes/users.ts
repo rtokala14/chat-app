cec0d7d419c53d3f23dcbd88ff9917a98ef762c5
Rohit Tokala <rtokala@purdue.edu>
2023-01-26 15:51:29 -0500

    Server- Login route

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -23,4 +23,24 @@ export async function userRoutes(app: FastifyInstance) {
       await streamChat.upsertUser({ id, name, image });
     }
   );
+
+  app.post<{ Body: { id: string } }>("/login", async (req, res) => {
+    const { id } = req.body;
+    if (id == null || id === "") {
+      return res.status(400).send();
+    }
+
+    const {
+      users: [user],
+    } = await streamChat.queryUsers({ id });
+
+    if (user == null) return res.status(401).send();
+
+    const token = streamChat.createToken(id);
+
+    return {
+      token,
+      user: { name: user.name, id: user.id, image: user.image },
+    };
+  });
 }
