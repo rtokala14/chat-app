923bd17e610ffe5c1a3c62cbbe232a017dba5c1c
Rohit Tokala <rtokala@purdue.edu>
2023-01-26 14:11:27 -0500

    Add form layout for signup

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,5 +1,24 @@
+import { useRef } from "react";
+import Input from "../components/Input";
+
 function SignUp() {
-  return <div>SignUp</div>;
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const imageUrlRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <h1 className=" text-3xl font-bold mb-8 text-center">SignUp</h1>
+      <form className=" grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
+        <label htmlFor="userName">Username</label>
+        <Input id="userName" pattern="\S*" required ref={usernameRef} />
+        <label htmlFor="name">Name</label>
+        <Input id="name" required ref={nameRef} />
+        <label htmlFor="imageUrl">Image Url</label>
+        <Input id="imageUrl" type={"url"} ref={imageUrlRef} />
+      </form>
+    </>
+  );
 }
 
 export default SignUp;
